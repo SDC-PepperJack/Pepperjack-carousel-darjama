64c577bbe2bf30918ee05172fa8b1ced76a55ad2
Daryl Marco <darmarco@gmail.com>
2019-11-02 17:13:39 -0700

    Fixed crashes on importing CSV, added async to pgSeed

--- server/seed/pgSeed.js
+++ server/seed/pgSeed.js
@@ -1,38 +1,30 @@
 const fs = require('fs');
-// const { Pool, Client } = require('pg');
 const faker = require('faker');
-// const models = require('../models/index.js');
 const imageList = require('./imageList.js');
 
 const imagePrefix = 'https://btetsy-carousel1.s3.us-east-2.amazonaws.com/';
 const images = imageList.ilist();
-// const pool = new Pool();
 
-
-const productSeed = () => {
+async function productSeed() {
   const imagesLength = images.length;
-  for (let j = 1; j <= 25; j += 1) {
+  for (let j = 1; j <= 10; j += 1) {
     let buffer = '';
-    for (let i = 0 + (j - 1) * 400000; i < 400000 * j; i += 1) {
+    for (let i = 0 + (j - 1) * 1000000; i < 1000000 * j; i += 1) {
       const randomProductItem = faker.commerce.productName();
-      const likeArray = [true, false];
       const productArray = [];
-      const randomNum = (max) => Math.floor(Math.random() * max);
+      const randomNum = (max) => Math.round(Math.random() * max);
       const randomNumUpTo4 = randomNum(4);
-      const randomNumUpTo2 = randomNum(2);
-      for (let j = 0; j < randomNum(randomNumUpTo4) + 1; j += 1) {
+      for (let x = 0; x < randomNumUpTo4 + 1; x += 1) {
         productArray.push(imagePrefix + images[randomNum(imagesLength)]);
       }
       const picArray = `"{${productArray.join(',')}}"`;
-      buffer += `${i}, ""${randomProductItem}"", ${picArray},${likeArray[randomNumUpTo2]}\n`;
+      buffer += `${i}, ""${randomProductItem}"", ${picArray},${i % 3 === 0}\n`;
       // models.saveProduct(i, randomProductItem, productArray, likeArray[randomNumUpTo2]);
     }
-    fs.writeFile(`pgProductRecordImport${100 + j}.csv`, buffer, (err) => {
-      if (err) throw err;
-      console.log('file saved!');
-    });
+    const result = await fs.writeFileSync(`./csv/pgProductImport${1000 + j}.csv`, buffer);
+    console.log(result + j);
   }
-};
+}
 
 // const wishlistSeed = () => {
 //   models.saveWishlist(['BTS BT21 Official Pyjamas Set', 'BTS - Bunny Hat Series Enamel Pin'], 'KIMTAEHYUNG');
@@ -40,3 +32,7 @@ const productSeed = () => {
 
 productSeed();
 // wishlistSeed();
+/*
+NOTE: MAY NEED TO SET MAX_OLD_SPACE_SIZE AS BELOW WHEN RUNNING THIS FILE
+node --max_old_space_size=8192 pgSeed.js
+*/
